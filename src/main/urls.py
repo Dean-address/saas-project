a7e2eb619e8443e38b979deab42cb7d42a243dd8
Alabi Khalid <alabikhalid.12@gmail.com>
2024-12-10 14:30:55 +0100

    created a view and routed it

--- src/main/urls.py
+++ src/main/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for main project.
+URL configuration for cfehome project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
+from .views import home_view, about_view
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", home_view),  # index page -> root page
+    path("about/", about_view),
+    path("hello-world/", home_view),
+    path("hello-world.html", home_view),
+    path("admin/", admin.site.urls),
 ]
